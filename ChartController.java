04d2596a595f9909446b6e441da04fc4c95510ee
IT 여행자 <66349648+hiparkwg@users.noreply.github.com>
2025-03-08 01:59:12 +0900

    Update ChartController.java

--- ChartController.java
+++ ChartController.java
@@ -1,4 +1,4 @@
-package kr.jobtc.restful_chartjs;
+package kr.jobtc.restfulandchartjs;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,36 +11,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @RestController
 public class ChartController {
-
+    
     @Autowired
     ChartDto dto;
 
-
     @GetMapping("/")
-    public ModelAndView chart() {
+    public ModelAndView chart(){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("chart");
+        mv.setViewName("chart"); // templates/chart.html
         return mv;
     }
 
+    @PostMapping("loadData")
+    public Map<String, Object> loadData(@RequestBody Map<String, String> param){
+        Map<String, Object> returnMap = new HashMap<>();
+        List<Map<String, Object>> data = null;
 
-    @PostMapping("/listData")
-    public Map<String, Object> listData(@RequestBody Map<String, String> param){
-        String byList = param.get("byList");
-        Map<String, Object> resp = new HashMap<>();
-        List<Map<String, Object>> data=null;
-        System.out.println(param);
-
+        String byList =param.get("byList");
         if(byList.equals("월별")){
-            data = dto.getListData();
-        }else if(byList.equals("분기별")){
+            data = dto.getMonthData();
+        }else{ // 분기별
             data = dto.getQuateData();
         }
-        
-        resp.put("result", data);
-        return resp;
+
+        returnMap.put("result", data);
+        return returnMap;
     }
+    
 }
