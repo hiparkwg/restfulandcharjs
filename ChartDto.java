cced7f4fa30b51e0e18e3d221d2c0e3d0967d243
IT 여행자 <66349648+hiparkwg@users.noreply.github.com>
2025-03-08 01:59:49 +0900

    Update ChartDto.java

--- ChartDto.java
+++ ChartDto.java
@@ -1,4 +1,4 @@
-package kr.jobtc.restful_chartjs;
+package kr.jobtc.restfulandchartjs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,56 +8,47 @@ import java.util.Random;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
-
 @Component
-@Data
 public class ChartDto {
-
-    String[] bgColor = {"#f00", "#0f0","#00f","#f0f"};
+    String[] bgColor ={"#f00","#0f0","#00f", "#f0f"}; //분기별 색상
     List<Map<String, Object>> data;
 
-    public List<Map<String, Object>> getListData(){ // database 역활
+    public List<Map<String, Object>> getMonthData(){ // 월별 데이터
         data = new ArrayList<>();
-        // 월별 데이터 
-        Random rnd = new Random();
-        for(int month=1 ; month<=12; month++){
+        Random rnd = new Random(); //월
+        for(int month = 1; month<=12 ; month++){
             int sale = rnd.nextInt(191)+10; // 10~200
-            int color = (int)(Math.ceil(month/3.)-1);
-            data.add(getMonthData(month,  sale, bgColor[color]));
-        }
+            int color = (int)Math.ceil(month/3.0)-1;//0~3
 
+            Map<String, Object> map = new HashMap<>();
+            map.put("month", month );
+            map.put("sale", sale);
+            map.put("bgColor", bgColor[color]);
+            data.add(map);
+        }
         return data;
-    }
-    public Map<String, Object> getMonthData(int month, int sale, String bgColor){
-        Map<String, Object> map = new HashMap<>();
-        map.put("month", month);
-        map.put("sale", sale);
-        map.put("bgColor", bgColor);
-        return map;
     }
 
     public List<Map<String, Object>> getQuateData(){
-        List<Map<String, Object>> quaterData = new ArrayList();
+        List<Map<String, Object>> quateData = new ArrayList<>();
         int[] saleData = new int[4];
-        if(data == null) data = getListData();
-
+        if(data == null) data = getMonthData();
         for(Map<String, Object> m : data){
             Integer month = (Integer)m.get("month");
             Integer sale = (Integer)m.get("sale");
-            Integer quater=(int)(Math.ceil(month/3.)-1);//1분기가 0 index
-
+            Integer quater = (int)Math.ceil(month/3.0)-1;
             saleData[quater] += sale;
         }
+
         for(int i=0 ; i<saleData.length ; i++){
             Map<String, Object> temp = new HashMap<>();
             temp.put("quater", (i+1));
             temp.put("sale", saleData[i]);
-            temp.put("bgColor", bgColor[i]);
-            quaterData.add(temp);
+            quateData.add(temp);            
         }
 
-        return quaterData;
+        return quateData;
+
     }
 
 }
